ea2382344d325c69ade43034cd423f448669ee79
Varvara <ivs.tmb@mail.ru>
2023-09-14 15:46:52 +0300

    commit СТОРОНЫ СВЕТА

--- main.py
+++ main.py
@@ -1,70 +1,25 @@
-my_list = [int(x) for x in input().split()]
-x = my_list[0]
-y = my_list[1]
-z = my_list[2]
-if x > 0:
-    if y > 0:
-        if z > 0:
-            print('1 четверть')
-        elif z < 0:
-            print('5 четверть')
-        else:
-          print('Точка между 1 и 5 четвертями')
-    elif y < 0:
-        if z > 0:
-          print('4 четверть')
-        elif z < 0:
-            print('8 четверть')
-        else:
-          print('Точка между 4 и 8 четвертями')
-    else:
-      if z == 0:
-        print('Ось x')
-      elif z > 0:
-        print('Точка между 1 и 4 четвертями')
-      else:
-        print('Точка между 5 и 8 четвертями')
+a = 'СЕВЕР'
+b = 'ЮГ'
+c = 'ЗАПАД'
+d = 'ВОСТОК'
+x = 0
+y = 0
+n = 10000
+for i in range(n):
+    side = input()
+    if side == a:
+        num = int(input())
+        y += num
+    elif side == b:
+        num = int(input())
+        y -= num
+    elif side == c:
+        num = int(input())
+        x -= num
+    elif side == d:
+        num = int(input())
+        x += num
+    elif side == 'СТОП':
+        break
 
-elif x < 0:
-    if y > 0:
-        if z > 0:
-          print('2 четверть')
-        elif z < 0:
-          print('6 четверть')
-        else:
-          print('Точка между 2 и 6 четвертями')
-    elif y < 0:
-        if z > 0:
-          print('3 четверть')
-        elif z < 0:
-            print('7 четверть')
-        else:
-          print('Точка между 3 и 7 четвертями')
-    else:
-      if z == 0:
-        print('Ось x')
-      elif z > 0:
-        print('Точка между 2 и 3 четвертями')
-      else:
-        print('Точка между 6 и 7 четвертями')
-
-else:
-    if y == 0:
-        if z == 0:
-            print('Начало координат')
-        else:
-            print('Ось z')
-    elif y > 0:
-      if z == 0:
-        print('Ocь y')
-      elif z > 0:
-        print('Точка между 1 и 2 четвертями')
-      else:
-        print('Точка между 5 и 6 четвертями')
-    else:
-      if z == 0:
-        print('Ocь y')
-      elif z > 0:
-        print('Точка между 3 и 4 четвертями')
-      else:
-        print('Точка между 7 и 8 четвертями')
+print(x, ';', y)
